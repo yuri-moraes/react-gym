6e0db0209b37a0389267aa048efab8ed73400a18
Yuri Nogueira de Moraes <yuri.nogueira.de.moraes@hotmail.com>
2024-08-09 12:48:03 -0300

    added togle-button-bg config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import Navbar from "@/scenes/navbar/index";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SelectedPage } from "./shared/types";
 
 const App: React.FC = () => {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [isTopOfPage, setIstopOfPage] = useState<boolean>(true)
+
+  useEffect(()=> {
+    const handleScroll = ()=> {
+      if (window.scrollY === 0) {
+        setIstopOfPage(true)
+        setSelectedPage(SelectedPage.Home)
+      } 
+      if (window.scrollY !== 0)  setIstopOfPage(false)
+  }
+  window.addEventListener("scroll", handleScroll);
+  return ()=> window.removeEventListener("scroll", handleScroll);
+}, [])
 
   return (
     <div className="app bg-gray-20">
       <Navbar
+      isTopOfPage={isTopOfPage}
          selectedPage={selectedPage}
          setSelectedPage={setSelectedPage}
       />
